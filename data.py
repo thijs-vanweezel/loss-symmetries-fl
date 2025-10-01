30100db38127cfa0b35117c312fcd3ea5d51d1f4
weezeltggv <thijs.vanweezel@tno.nl>
2025-10-01 11:39:15 +0000

    redo label skew by ordered sampling

--- data.py
+++ data.py
@@ -56,29 +56,40 @@ def perspective_shift(image, angle=0., severity=0.):
     return cv2.warpPerspective(image, transform, (w, h))
 
 class Imagenet(Dataset):
-    def __init__(self, data_path, indiv_lab, shared_lab):
-        # Get all image paths and corresponding labels
+    def __init__(self, data_path, indiv_lab, shared_lab, batch_size, n):
+        # Get all image paths and sort them by client/shared
         g = os.walk(data_path, topdown=True)
         self.classes = next(g)[1]
-        paths = {dirname.rsplit("/", 1)[-1]: [os.path.join(dirname, f) for f in filenames] for (dirname, _, filenames) in g}
-        # Order in such a way that the classes alternate between clients
-        shuffle = np.dstack(indiv_lab).flatten()
-        shuffle = np.insert(shuffle, np.linspace(0, len(shuffle), len(shared_lab), dtype=int), shared_lab)
-        self.paths = []
-        while all(map(lambda v: bool(v), paths.values())): # NOTE: this condition makes the dataset balanced but also smaller
-            self.paths += [paths[self.classes[c]].pop() for c in shuffle]
+        classesbyclient = {c: [self.classes[i] for i in indiv_lab[c]] for c in range(n)}
+        sharedclasses = [self.classes[i] for i in shared_lab]
+        paths = [os.path.join(dirname, f) for (dirname, _, filenames) in g for f in filenames]
+        self.pathsbyclient = {client: [file for file in paths if file.rsplit("/")[-2] in classesbyclient[client]] for client in range(n)}
+        self.sharedpaths = [file for file in paths if file.rsplit("/")[-2] in sharedclasses]
+        self.n = n
+        self.batch_size = batch_size
+        self.frac = len(indiv_lab[0])/len(self.classes)
 
     def __len__(self):
-        return len(self.paths)
+        return min(len(v) for v in self.pathsbyclient.values())*self.n
 
     def __getitem__(self, idx):
-        fp = self.paths[idx]
+        # If the sample index within this batch is less than the required number of individual samples
+        required_indiv = self.n*self.frac*self.batch_size
+        required_indiv = int(required_indiv - required_indiv%self.n)
+        required_shared = self.batch_size-required_indiv
+        idx_in_batch = idx-idx//self.batch_size*self.batch_size
+        if idx_in_batch < required_indiv:
+            # Fetch the next individual sample
+            fp = self.pathsbyclient[idx%self.n][(idx-required_shared)//self.n]
+        else:
+            # Fetch a shared sample
+            fp = self.sharedpaths[idx-int(required_indiv)]
         img = torchvision.io.read_image(fp).float()/255.
         label = self.classes.index(fp.split("/")[-2].rstrip(".JPEG"))
-        label = torch.eye(1000)[label]
+        label = torch.eye(len(self.classes))[label]
         return img, label
 
-def jax_collate(batch, n, key, feature_beta, label_beta, sample_overlap, indiv_lab, shared_lab):
+def jax_collate(batch, n, feature_beta, indiv_stop, sample_overlap):
     """
     Custom collate, necessary for several reasons: 
     - Images have different sizes, and batches can only contain one shape
@@ -94,18 +105,14 @@ def jax_collate(batch, n, key, feature_beta, label_beta, sample_overlap, indiv_l
     # Convert and concat
     imgs = np.swapaxes(np.stack([np.asarray(img, dtype=np.float32) for img in imgs]), 1, -1) # HWC
     labels = jnp.stack([jnp.asarray(label, dtype=jnp.float32) for label in labels])
-    globals()["imgs"] = imgs # DEBUG
-    globals()["labels"] = labels # DEBUG
 
-    # Create label assignment
     # Fetch corresponding idx in this batch
-    clients_masks = [jnp.isin(labels.argmax(-1), jnp.concat([indiv_lab[i], shared_lab])) for i in range(n)]
-    clients_imgs = [imgs[clients_masks[i]] for i in range(n)]
-    print([clients_imgs[i].shape for i in range(n)]) # DEBUG
-    clients_labels = jnp.stack([labels[clients_masks[i]] for i in range(n)], axis=0)
+    indiv_stop = int(indiv_stop - indiv_stop%n)
+    clients_idxs = [list(range(i, indiv_stop, n))+list(range(indiv_stop, len(imgs))) for i in range(n)]
+    clients_imgs = [imgs[clients_idxs[i]] for i in range(n)]
+    clients_labels = jnp.stack([labels[jnp.asarray(clients_idxs[i])] for i in range(n)], axis=0)
 
     # Decrease sample overlap (doing this batch-wise better reflects the overlap within the gradient)
-
 
     # Create feature skew with elastic deformation
     angles = [i*2*np.pi/n for i in range(n)]
@@ -115,23 +122,22 @@ def jax_collate(batch, n, key, feature_beta, label_beta, sample_overlap, indiv_l
 
     return clients_imgs, clients_labels
 
-def create_imagenet(path="./data/Data/CLS-LOC/train", n=4, key=jax.random.key(42), feature_beta=0., label_beta=0., sample_overlap=1., batch_size=64, **kwargs):
+def create_imagenet(path="./data/Data/CLS-LOC/train", n=4, feature_beta=0., label_beta=0., sample_overlap=1., batch_size=64, **kwargs):
     assert label_beta==0. or sample_overlap==1., "Sample overlap reduction not implemented for label_beta>0"
     assert label_beta==0. or batch_size%n==0, "Original batch size must be divisible by n, so that labels can be evenly distributed"
-    # Create label assignment 
-    # Fractions derived from ( sha + n * ind = 1 ) and ( ind / (sha + ind) = label_beta )
+    # Label skew fractions derived from ( sha + n * ind = 1 ) and ( ind / (sha + ind) = label_beta )
     shared_frac = 1/(1+n*label_beta/(1-label_beta))
     indiv_frac = label_beta/(1-label_beta)*shared_frac
-    # Convert to labels (assuming 1000 classes)
+    # Create label assignment (assuming 1000 classes)
     indiv_lab = [jnp.arange(i*int(1000*indiv_frac), (i+1)*int(1000*indiv_frac)) for i in range(n)]
     shared_lab = jnp.arange(n*int(1000*indiv_frac), 1000)
     # Recalculate batch size
-    print(batch_size, shared_frac, indiv_frac) # DEBUG
-    batch_size = 104 # DEBUG, should be int(batch_size * (shared_frac + indiv_frac))
+    batch_size = batch_size / (shared_frac + indiv_frac)
+    batch_size = int(batch_size - batch_size%n)
     return DataLoader(
-        Imagenet(path, indiv_lab, shared_lab),
+        Imagenet(path, indiv_lab, shared_lab, batch_size, n),
         batch_size=batch_size,
-        collate_fn=partial(jax_collate, n=n, key=key, feature_beta=feature_beta, label_beta=label_beta, sample_overlap=sample_overlap, indiv_lab=indiv_lab, shared_lab=shared_lab),
+        collate_fn=partial(jax_collate, n=n, feature_beta=feature_beta, indiv_stop=len(indiv_lab[0])/1000*batch_size*n, sample_overlap=sample_overlap),
         shuffle=False,
         **kwargs
     )
