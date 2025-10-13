cc6f273b94c12e838a3a3949546182e5afcc6989
weezeltggv <thijs.vanweezel@tno.nl>
2025-10-13 12:34:10 +0000

    return individual models

--- fedflax.py
+++ fedflax.py
@@ -23,7 +23,7 @@ def get_updates(model_g, models):
     params_g = jax.tree.leaves(nnx.to_tree(model_g))
     return jax.tree.map(lambda pg, p: p-pg, params_g, params) # list of length n_layers with arrays of shape (n_clients, layer_shape)
 
-def aggregate(model_g, updates, n):
+def aggregate(model_g, updates):
     # Get model structure
     params_g, struct = jax.tree.flatten(nnx.to_tree(model_g))
     # Average updates
@@ -32,10 +32,14 @@ def aggregate(model_g, updates, n):
     params_g = jax.tree.map(lambda pg, u: pg + u, params_g, update)
     # Convert to model
     model_g = nnx.from_tree(jax.tree.unflatten(struct, params_g))
-    # Broadcast to n models
+    return model_g
+
+# Broadcast global model to clients
+def cast(model_g, n):
+    params_g, struct = jax.tree.flatten(nnx.to_tree(model_g))
     params_all = jax.tree.map(lambda x: jnp.repeat(jnp.expand_dims(x, 0), n, 0), params_g)
     models = nnx.from_tree(jax.tree.unflatten(struct, params_all))
-    return model_g, models
+    return models
 
 def train(model, opt_create, ds_train, ds_val, ell, local_epochs, filename=None, n=4, max_patience=5, max_rounds=jnp.inf):
     # Identically initialized models, interpretable as collection by nnx
@@ -59,6 +63,8 @@ def train(model, opt_create, ds_train, ds_val, ell, local_epochs, filename=None,
     r = 0
     patience = 1
     while r<=max_rounds and (max_patience is None or r<=1 or patience<=max_patience):
+        # Re-initialize models to global model
+        models = cast(model_g, n)
         # Local training
         losses = jnp.concat([losses, jnp.zeros((1,n+1))])
         for epoch in range(local_epochs):
@@ -67,13 +73,13 @@ def train(model, opt_create, ds_train, ds_val, ell, local_epochs, filename=None,
                 with NpyAppendArray(filename, delete_if_exists=True if epoch+r==0 else False) as f:
                     f.append(np.concat([p.reshape(n,-1) for p in jax.tree.leaves(nnx.to_tree(models))], axis=1))
             # Iterate over batches
-            for b, (x_batch, z_batch, y_batch) in enumerate(tqdm(ds_train, leave=False, desc=f"Round {r} Epoch {epoch}/{local_epochs-1}")):
+            for b, (x_batch, z_batch, y_batch) in enumerate(tqdm(ds_train, leave=False, desc=f"Round {r} Epoch {epoch+1}/{local_epochs}")):
                 loss = train_step(models, model_g, opts, x_batch, z_batch, y_batch)
                 if jnp.isnan(loss).any():
                     print(losses)
                     raise ValueError("NaN encountered")
                 losses = losses.at[-1,:-1].set(losses[-1,:-1] + loss)
-        # Aggregate and evaluate
+        # Evaluate
         losses = losses.at[-1,:-1].set(losses[-1,:-1]/local_epochs/(b+1))
         val_loss = reduce(lambda a,b: a+ell_val(models, model_g, *b)[0].mean(), ds_val, 0.)
         val_loss /= len(ds_val)
@@ -81,7 +87,7 @@ def train(model, opt_create, ds_train, ds_val, ell, local_epochs, filename=None,
         print(f"round {r} global validation loss: {val_loss}")
         # Aggregate
         updates = get_updates(model_g, models)
-        model_g, models = aggregate(model_g, updates, n)
+        model_g = aggregate(model_g, updates)
         # Check if model is converged
         r += 1
         if r>1 and losses[-1,-1]>losses[-patience-1,-1]:
@@ -91,7 +97,7 @@ def train(model, opt_create, ds_train, ds_val, ell, local_epochs, filename=None,
     # Save final params
     if filename:
         with NpyAppendArray(filename) as f:
-            f.append(np.concat([p.reshape(n,-1) for p in jax.tree.leaves(nnx.to_tree(models))], axis=1))
+            f.append(np.concat([p.reshape(n,-1) for p in jax.tree.leaves(nnx.to_tree(cast(models, n)))], axis=1))
 
     # Returns all kinds of output for the various analyses
-    return updates
+    return updates, models
