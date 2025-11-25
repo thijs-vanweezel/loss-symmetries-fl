7099a081ab8b2f25dd900da1584281a095d11f08
weezeltggv <thijs.vanweezel@tno.nl>
2025-11-25 12:03:31 +0100

    remove pfree in favor of bool

--- models.py
+++ models.py
@@ -28,15 +28,16 @@ def mask_linear(in_dim, out_dim, **kwargs):
 # W-Asymmetry implementation consistent with https://github.com/cptq/asymmetric-networks/blob/main/lmc/models/models_mlp.py#L169 
 # And SyRe implementation consistent with https://github.com/xu-yz19/syre/blob/main/MLP.ipynb
 class AsymLinear(nnx.Linear):
-    def __init__(self, key:jax.dtypes.prng_key, pfree:float=1., sigma:float=0., **kwargs):
+    def __init__(self, key:jax.dtypes.prng_key, wasym:bool=False, kappa:float=1., sigma:float=0., **kwargs):
         keys = jax.random.split(key, 4)
         super().__init__(rngs=nnx.Rngs(keys[0]), **kwargs)
         # Check if asymmetry is to be applied
         self.ssigma = sigma
-        self.wasym = pfree<1.
+        self.wasym = wasym
+        self.kappa = kappa
         # Create params
-        if self.wasym: self.wmask = mask_linear(*self.kernel.shape, dtype=self.param_dtype)
-        if sigma>0. or self.wasym: self.randk = jax.random.normal(keys[2], self.kernel.shape, dtype=self.param_dtype)
+        if wasym: self.wmask = mask_linear(*self.kernel.shape, dtype=self.param_dtype)
+        if sigma>0. or wasym: self.randk = jax.random.normal(keys[2], self.kernel.shape, dtype=self.param_dtype)
         if sigma>0.: self.randb = jax.random.normal(keys[3], self.bias.shape, dtype=self.param_dtype)
 
     def __call__(self, inputs:jax.Array) -> jax.Array:
@@ -48,7 +49,7 @@ class AsymLinear(nnx.Linear):
             kernel = kernel + self.randk * self.ssigma
         # Apply w-asymmetry
         if self.wasym:
-            kernel = kernel * self.wmask + (1-self.wmask) * self.randk # TODO: experiment with decreased std
+            kernel = kernel * self.wmask + (1-self.wmask) * self.randk * self.kappa
         # Implementation directly copied from nnx.Linear
         inputs, kernel, bias = self.promote_dtype(
             (inputs, kernel, bias), dtype=self.dtype
@@ -65,14 +66,14 @@ class AsymLinear(nnx.Linear):
 # Creates mask with the maximum number of non-zero weights while ensuring that each row is unique
 # Supports only square kernels
 # https://github.com/cptq/asymmetric-networks/blob/main/lmc/models/models_resnet.py#L173
-def mask_conv(in_channels, out_channels, kernel_size, **kwargs):
+def mask_conv(kernel_size, in_channels, out_channels, **kwargs):
     mask = jnp.ones((kernel_size, kernel_size, in_channels, out_channels), **kwargs)
     weights_per_out_channel = in_channels * kernel_size**2
     flat_idx_to_3d_idx = jax.vmap(lambda idx : [idx%kernel_size, (idx//kernel_size)%kernel_size, idx//kernel_size**2])
     out_channel_idx = 1
 
     for num_zeros in range(1, weights_per_out_channel):
-        flat_cols_idxs = combinations(range(weights_per_out_channel), num_zeros) # not actually column, but rather (in_c, k_h, k_w)
+        flat_cols_idxs = combinations(range(weights_per_out_channel), num_zeros) # not actually column, but rather (k_h, k_w, in_c)
         cols_idxs = map(jnp.array, flat_cols_idxs)
         cols_idxs = map(flat_idx_to_3d_idx, cols_idxs)
         for cols_idx in cols_idxs:
@@ -83,15 +84,16 @@ def mask_conv(in_channels, out_channels, kernel_size, **kwargs):
 
 # W-Asymmetry implementation consistent with https://github.com/cptq/asymmetric-networks/blob/main/lmc/models/models_resnet.py#L22
 class AsymConv(nnx.Conv):
-    def __init__(self, key:jax.dtypes.prng_key, pfree:float=1., sigma:float=0., **kwargs):
+    def __init__(self, key:jax.dtypes.prng_key, wasym:bool=False, kappa:float=1., sigma:float=0., **kwargs):
         keys = jax.random.split(key, 4)
         super().__init__(rngs=nnx.Rngs(keys[0]), **kwargs)
         # Check if asymmetry is to be applied
         self.ssigma = sigma
-        self.wasym = pfree<1.
+        self.wasym = wasym
+        self.kappa = kappa
         # Create params
-        if self.wasym: self.wmask = mask_conv(*self.kernel.shape[1:], dtype=self.param_dtype)
-        if sigma>0. or self.wasym: self.randk = jax.random.normal(keys[2], self.kernel.shape, dtype=self.param_dtype)
+        if wasym: self.wmask = mask_conv(*self.kernel.shape[1:], dtype=self.param_dtype)
+        if sigma>0. or wasym: self.randk = jax.random.normal(keys[2], self.kernel.shape, dtype=self.param_dtype)
         if sigma>0.: self.randb = jax.random.normal(keys[3], self.bias.shape, dtype=self.param_dtype)
 
     def __call__(self, inputs:jax.Array) -> jax.Array:
@@ -103,7 +105,7 @@ class AsymConv(nnx.Conv):
             kernel = kernel + self.randk * self.ssigma
         # Apply w-asymmetry
         if self.wasym:
-            kernel = kernel * self.wmask + (1-self.wmask) * self.randk # TODO: experiment with decreased std
+            kernel = kernel * self.wmask + (1-self.wmask) * self.randk * self.kappa
         # Implementation directly copied from nnx.Conv
         inputs, kernel, bias = self.promote_dtype(
             (inputs, kernel, bias), dtype=self.dtype
@@ -128,13 +130,14 @@ class AsymConv(nnx.Conv):
 
 # Used in resnet
 class ResNetBlock(nnx.Module):
-    def __init__(self, key:jax.dtypes.prng_key, in_kernels:int, out_kernels:int, stride:int=1, pfree=1., sigma=0.):
+    def __init__(self, key:jax.dtypes.prng_key, in_kernels:int, out_kernels:int, stride:int=1, wasym:bool=False, kappa:float=1., sigma:float=0.):
         super().__init__()
         keys = jax.random.split(key, 5)
         self.stride = stride
         self.conv1 = AsymConv(
             keys[0],
-            pfree,
+            wasym,
+            kappa,
             sigma,
             in_features=in_kernels,
             out_features=out_kernels,
@@ -147,7 +150,8 @@ class ResNetBlock(nnx.Module):
         self.norm1 = nnx.BatchNorm(out_kernels, rngs=nnx.Rngs(keys[1]), param_dtype=jnp.float32, dtype=jnp.bfloat16)
         self.conv2 = AsymConv(
             keys[2],
-            pfree,
+            wasym,
+            kappa,
             sigma,
             in_features=out_kernels,
             out_features=out_kernels,
@@ -158,7 +162,7 @@ class ResNetBlock(nnx.Module):
         )
         self.norm2 = nnx.BatchNorm(out_kernels, rngs=nnx.Rngs(keys[3]), param_dtype=jnp.float32, dtype=jnp.bfloat16)
         if stride>1 and in_kernels!=out_kernels:
-            self.id_conv = AsymConv(keys[4], pfree, sigma, in_features=in_kernels, out_features=out_kernels, 
+            self.id_conv = AsymConv(keys[4], wasym, kappa, sigma, in_features=in_kernels, out_features=out_kernels, 
                                     kernel_size=(1,1), strides=(stride, stride), dtype=jnp.bfloat16, param_dtype=jnp.bfloat16)
 
     def __call__(self, x, train=True):
@@ -173,22 +177,22 @@ class ResNetBlock(nnx.Module):
 
 # Resnet for ImageNet ([3,4,6,3] for 34 layers, [2,2,2,2] for 18 layers)
 class ResNet(nnx.Module): # TODO: 36/(2**5) is a small shape for conv
-    def __init__(self, key:jax.dtypes.prng_key, block=ResNetBlock, layers=[2,2,2,2], kernels=[64,128,256,512], channels_in=1, dim_out=9, dimexp=False, pfree=1., sigma=0., **kwargs):
+    def __init__(self, key:jax.dtypes.prng_key, block=ResNetBlock, layers=[2,2,2,2], kernels=[64,128,256,512], channels_in=1, dim_out=9, dimexp=False, wasym:bool=False, kappa:float=1., sigma=0., **kwargs):
         super().__init__(**kwargs)
         # Dimension expansion params
         self.dimexp = dimexp
         # Keys
         keys = jax.random.split(key, sum(layers)+2)
         # Layers
-        self.conv = AsymConv(keys[0], pfree, sigma, in_features=channels_in, out_features=64, kernel_size=(7,7), strides=(2,2), padding="SAME", param_dtype=jnp.bfloat16, dtype=jnp.bfloat16)
+        self.conv = AsymConv(keys[0], wasym, kappa, sigma, in_features=channels_in, out_features=64, kernel_size=(7,7), strides=(2,2), padding="SAME", param_dtype=jnp.bfloat16, dtype=jnp.bfloat16)
         self.layers = []
         for j, l in enumerate(layers):
             for i in range(l):
                 k_in = ([64]+kernels)[j] if i==0 else kernels[j]
                 k_out = kernels[j]
                 s = 2 if i==0 and j>0 else 1
-                self.layers.append(block(keys[j+(i*j)], k_in, k_out, stride=s, pfree=pfree, sigma=sigma))
-        self.fc = AsymLinear(keys[-1], pfree, sigma, in_features=kernels[-1]+3, out_features=dim_out, param_dtype=jnp.bfloat16, dtype=jnp.bfloat16)
+                self.layers.append(block(keys[j+(i*j)], k_in, k_out, stride=s, wasym=wasym, kappa=kappa, sigma=sigma))
+        self.fc = AsymLinear(keys[-1], wasym, kappa, sigma, in_features=kernels[-1]+3, out_features=dim_out, param_dtype=jnp.bfloat16, dtype=jnp.bfloat16)
 
     def __call__(self, x, z, train=True):
         # Apply asymmetries (syre before wasym to avoid biasing the masks)
@@ -205,18 +209,18 @@ class ResNet(nnx.Module): # TODO: 36/(2**5) is a small shape for conv
 
 # LeNet-5 for 36X60 images + 3 auxiliary features
 class LeNet(nnx.Module):
-    def __init__(self, key:jax.dtypes.prng_key, dimexp=False, pfree=1., dim_out=9, sigma=0.):
+    def __init__(self, key:jax.dtypes.prng_key, dimexp=False, wasym=False, kappa=1., dim_out=9, sigma=0.):
         super().__init__()
         # Dimension expansion params
         flat_shape = 15*27 if dimexp else 6*12
         self.dimexp = dimexp
         # Layers
         keys = jax.random.split(key, 5)
-        self.conv1 = AsymConv(keys[0], pfree, sigma, in_features=1, out_features=8, kernel_size=(4,4), padding="VALID")
-        self.conv2 = AsymConv(keys[1], pfree, sigma, in_features=8, out_features=16, kernel_size=(4,4), padding="VALID")
-        self.fc1 = AsymLinear(keys[2], pfree, sigma, in_features=flat_shape*16+3, out_features=128)
-        self.fc2 = AsymLinear(keys[3], pfree, sigma, in_features=128, out_features=64)
-        self.fc3 = AsymLinear(keys[4], pfree, sigma, in_features=64, out_features=dim_out)
+        self.conv1 = AsymConv(keys[0], wasym, kappa, sigma, in_features=1, out_features=8, kernel_size=(4,4), padding="VALID")
+        self.conv2 = AsymConv(keys[1], wasym, kappa, sigma, in_features=8, out_features=16, kernel_size=(4,4), padding="VALID")
+        self.fc1 = AsymLinear(keys[2], wasym, kappa, sigma, in_features=flat_shape*16+3, out_features=128)
+        self.fc2 = AsymLinear(keys[3], wasym, kappa, sigma, in_features=128, out_features=64)
+        self.fc3 = AsymLinear(keys[4], wasym, kappa, sigma, in_features=64, out_features=dim_out)
     
     def __call__(self, x, z, train=None):
         # Apply dimension expansion if necessary
