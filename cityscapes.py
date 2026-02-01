7b9b376c59541cbe90bb9df48b7eac57b25ec86b
weezeltggv <thijs.vanweezel@tno.nl>
2026-02-01 21:07:55 +0000

    add miou to loss

--- cityscapes.py
+++ cityscapes.py
@@ -1,14 +1,15 @@
 # Imports
+import jax, optax, pickle
 from fedflax import train
 from models import UNETR
 from data import fetch_data
-from utils import return_ce, mean_iou_err
-import jax, optax, pickle
+from utils import miou
+from jax import numpy as jnp
 from flax import nnx
-from functools import reduce
+from functools import partial, reduce
 
 # Configuration
-n_clients = 3
+n_clients = 1
 asymkwargs = {}
 
 # Initialize model
@@ -25,11 +26,16 @@ ds_train = fetch_data(beta=1., dataset=2, n_clients=n_clients, batch_size=16)
 ds_val = fetch_data(beta=1., dataset=2, partition="val", n_clients=n_clients, batch_size=16)
 
 # Train (fixed number of epochs since test data is not available)
+def loss_fn(model, model_g, y, *xs):
+    logits = model(*xs, train=True)
+    ce = optax.softmax_cross_entropy(logits, y, axis=-1).mean()
+    miou_err = 1 - miou(logits, y)
+    return ce + miou_err
 models, rounds = train(
     model,
     opt,
     ds_train,
-    return_ce(0.), 
+    loss_fn, 
     local_epochs=50,
     n_clients=n_clients,
     rounds=1
@@ -40,16 +46,20 @@ struct, state, rest = nnx.split(models, (nnx.Param, nnx.BatchStat), ...)
 state_g = jax.tree.map(lambda x: x.mean(0), state)
 model_g = nnx.merge(struct, state_g, rest)
 
-# Evaluate aggregated model
-vval_fn = nnx.jit(nnx.vmap(mean_iou_err, in_axes=(None,0,0)))
-err_g = reduce(lambda acc, batch: acc + vval_fn(model_g, *batch), ds_val, 0.) / len(ds_val)
-print("Global mIoU: ", err_g.mean().item())
-
-# Evaluate client models separately
-vval_fn = nnx.jit(nnx.vmap(mean_iou_err))
-err_l = reduce(lambda acc, batch: acc + vval_fn(models, *batch), ds_val, 0.) / len(ds_val)
-print("Local mIoU: ", err_l.mean().item())
-
 # Save client models
 state = nnx.state(models, ...)
-pickle.dump(state, open("models/cs_central.pkl", "wb"))
+pickle.dump(state, open("models/cs_central_.pkl", "wb"))
+
+# Evaluate aggregated model
+vval_fn = nnx.jit(nnx.vmap(
+    lambda model, y, *xs: miou(model(*xs, train=False), y, partial(jnp.argmax, axis=-1)), 
+    in_axes=(None,0,0)))
+miou_g = reduce(lambda acc, batch: acc + vval_fn(model_g, *batch), ds_val, 0.) / len(ds_val)
+print("Global mIoU: ", miou_g.mean().item())
+
+# Evaluate client models separately
+vval_fn = nnx.jit(nnx.vmap(
+    lambda model, y, *xs: miou(model(*xs, train=False), y, partial(jnp.argmax, axis=-1))
+    ))
+miou_l = reduce(lambda acc, batch: acc + vval_fn(models, *batch), ds_val, 0.) / len(ds_val)
+print("Local mIoU: ", miou_l.mean().item())
