b516977945e5845889a5ab1e9f15694c6a8e72d6
Thijs van Weezel <117186594+thijs-vanweezel@users.noreply.github.com>
2026-02-15 10:57:29 +0100

    Fix if-statements

--- download_data.sh
+++ download_data.sh
@@ -4,7 +4,6 @@ set -euo pipefail
 # -- OXFORD-IIIT PETS DOWNLOAD --
 if [ -d oxford_pets ] && [ "$(ls -A oxford_pets/images 2>/dev/null)" ]; then
     echo "Oxford-IIIT Pets dataset already present. Skipping download."
-fi
 else
     mkdir -p oxford_pets
     
@@ -13,14 +12,14 @@ else
     
     tar -xf oxford_pets/images.tar.gz -C oxford_pets
     tar -xf oxford_pets/annotations.tar.gz -C oxford_pets
+fi
 
 # -- IMAGENET DOWNLOAD --
-if [ -d imagenet ] && [ "$(ls -A imagenet/ILSVRC 2>/dev/null)" ]
+if [ -d imagenet ] && [ "$(ls -A imagenet/ILSVRC 2>/dev/null)" ]; then
     echo "ImageNet already present. Skipping download."
 else
     mkdir -p imagenet
     cd ./imagenet
-    COOKIE_JAR=
     
     curl -c cookies.txt -sSL https://image-net.org/login -o login_page.html
     
@@ -37,3 +36,4 @@ else
     tar -xzf data.tar.gz
     
     cd ..
+fi
